94c4a08ae45e8ae4842bebb2f042f7c9f712fbbc
Estu <estulpzlet@gmail.com>
2025-11-07 21:22:14 +0000

    refactor(tests): add more units tests, fix mock tests.

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -1,10 +1,20 @@
 from src.translator import translate_content
 
-
-def test_chinese():
+def test_known_chinese():
     is_english, translated_content = translate_content("这是一条中文消息")
     assert is_english == False
     assert translated_content == "This is a Chinese message"
+
+def test_known_english():
+    is_english, translated = translate_content("This is an English message")
+    assert is_english is True
+    assert translated == "This is an English message"
+
+def test_unknown_fallback():
+    text = "Random unknown text 123"
+    is_english, translated = translate_content(text)
+    assert is_english is True
+    assert translated == text
 
 def test_llm_normal_response():
     pass
